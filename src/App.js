36ef16b677502ab73018fd97de93f7fb1627e550
gwatson86 <gwatson86@gmail.com>
2018-11-06 17:32:52 -0600

    working about as expected

--- src/App.js
+++ src/App.js
@@ -2,13 +2,82 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+const URL = "http://localhost:3000/pizzas"
 class App extends Component {
+  state = {
+    pizzas: [],
+    pizza: {}
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(pizzas => this.setState({ pizzas }))
+  }
+
+  populateEditForm = pizza => {
+    this.setState({ pizza })
+  }
+
+  changeTopping = event => {
+    let topping = event.target.value
+    this.setState(prevState => {
+      return {pizza: {...prevState.pizza, topping: topping}}
+    })
+  }
+
+  changeSize = event => {
+    let size = event.target.value
+    this.setState(prevState => {
+      return {pizza: {...prevState.pizza, size: size}}
+    })
+  }
+
+  changeVegetarian = event => {
+    let vegetarian = event.target.value === "Vegetarian"
+    this.setState(prevState => {
+      return {pizza: {...prevState.pizza, vegetarian: vegetarian}}
+    })
+  }
+
+  editPizza = pizza => {
+    const data = {
+      topping: pizza.topping,
+      size: pizza.size,
+      vegetarian: pizza.vegetarian
+    }
+
+    fetch(`${URL}/${pizza.id}`, {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      headers: {"Content-Type": "application/json"}
+    })
+    .then(response => response.json())
+    .then(pizza => {
+      const updatedPizzas = this.state.pizzas.map(p => {
+        if (pizza.id === p.id) {
+          return pizza;
+        } else {
+          return p;
+        }
+      })
+      this.setState({
+        pizzas: updatedPizzas
+      })
+    });
+  }
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm
+          pizza={this.state.pizza}
+          editPizza={this.editPizza}
+          changeTopping={this.changeTopping}
+          changeSize={this.changeSize}
+          changeVegetarian={this.changeVegetarian} />
+        <PizzaList pizzas={this.state.pizzas} populateEditForm={this.populateEditForm} />
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,16 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+  const pizza = props.pizza
   return(
       <div className="form-row">
         <div className="col-5">
             <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+                pizza.topping
+              } onChange={props.changeTopping}/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={pizza.size} className="form-control" onChange={props.changeSize}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,20 +18,20 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Vegetarian" checked={pizza.vegetarian} onChange={props.changeVegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={!pizza.vegetarian} onChange={props.changeVegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={() => {props.editPizza(pizza)}}>Submit</button>
         </div>
       </div>
 
